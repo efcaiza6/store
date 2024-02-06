e5c25fcdeabb547e03cf71b1407ef654ff0a236b
Edwin Fabricio Caiza Pilatasig <bocis9@outlook.com>
2024-02-06 16:37:47 -0500

    Implementation of signals and services

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
-fromChild(event: string){
-  console.log('estamos en el padre');
-  console.log(event);
-}
+  products = signal<Product[]>([]);
+  private cartService = inject(CartService);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=2',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 90,
+        image: 'https://picsum.photos/640/640?r=22',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
 }

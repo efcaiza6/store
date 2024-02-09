5e87e84d66649da270cf40ff0195c629e102830b
Edwin Fabricio Caiza Pilatasig <bocis9@outlook.com>
2024-02-09 11:58:49 -0500

    Validaciones, Css

--- src/app/domains/forms/components/basic-form/basic-form.component.ts
+++ src/app/domains/forms/components/basic-form/basic-form.component.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './basic-form.component.html',
   styleUrl: './basic-form.component.css',
 })
 export class BasicFormComponent {
-  nameField = new FormControl('');
+  nameField = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(10),
+  ]);
   emailField = new FormControl('');
   phoneField = new FormControl('');
   colorField = new FormControl('');
@@ -17,6 +21,9 @@ export class BasicFormComponent {
   numberField = new FormControl(12);
   categoryField = new FormControl('category-1');
   tagField = new FormControl('');
+  agreeField = new FormControl(false);
+  genderField = new FormControl('');
+  zoneField = new FormControl('');
 
   ngOnInit() {
     this.nameField.valueChanges.subscribe((value) => {
@@ -26,5 +33,13 @@ export class BasicFormComponent {
 
   getNameValue() {
     console.log(this.nameField.value);
+  }
+
+  get isNameFieldValid(){
+    return this.nameField.touched && this.nameField.valid
+  }
+
+  get isNameFieldInvalid(){
+    return this.nameField.touched && this.nameField.invalid
   }
 }
